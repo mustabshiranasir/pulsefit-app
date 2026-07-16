3e81813be374184e856a8faf0f1e0614ab570d80
Mustabshira <mustabshira.clicktake@gmail.com>
2026-07-15 23:05:18 -0700

    Fix layout to not wrap app in expo-router Stack

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,3 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-
-export default function RootLayout() {
-  return (
-    <>
-      <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
-  );
+export default function RootLayout({ children }) {
+  return children;
 }
